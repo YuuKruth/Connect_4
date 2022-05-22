48f4cded39d491838dac8e221ce0232991c86560
Yuu Kruth <kyuu962@gmail.com>
2022-05-22 17:41:59 +0200

    fixed input checker

--- connect_4.py
+++ connect_4.py
@@ -58,24 +58,34 @@ def winning_check(board, piece):
 
 board = create_board()
 print_board(board)
+#print("Notice -------> Type 'exit' to quit game.")
 game_over = False
 turn = 0
+
 
 while not game_over:
     # Ask Player 1 to input
     if turn == 0:
-
-        col = 0
+        #making sure that all inputs are correct
         while True:
-            col_test = int(input("Player 2 Make your selection (0-" + str(COLUMN_COUNT - 1) + "): "), 10)
-
-            if col_test == range(COLUMN_COUNT):
-                col = int(col_test)
-                break
+            
+            try:
+                col_test = int(input("Player 1 Make your selection (0-" + str(COLUMN_COUNT - 1) + "): "), 10)
                 
-            else:
-                print("ERROR INPUT -> Input has to be between 0 and " + str(COLUMN_COUNT - 1)))
-                print("please try again:") 
+                if 0 <= col_test and col_test < COLUMN_COUNT:
+                    
+                    col = int(col_test)
+                    break
+                
+                else:
+                    print("ERROR INPUT -> Input has to be between 0 and " + str(COLUMN_COUNT - 1))
+                    print("please try again:")
+
+            except ValueError:
+                print("ValueError  --> Input has to be numeric! (positive integer)")
+                print("ERROR INPUT --> Input has to be between 0 and " + str(COLUMN_COUNT - 1))
+                print("please try again:")
+                pass
 
 
         if is_valid_location(board, col):
@@ -90,16 +100,27 @@ while not game_over:
 
     # Ask Player 2 to input
     else:
-        try: #exit loop if player types nothing
-            col = int(input("Player 2 Make your selection (0-" + str(COLUMN_COUNT - 1) + "): "), 10)
+
+        #making sure that all inputs are correct
+        while True:
             
-            if col != range(COLUMN_COUNT):
+            try:
+                col_test = int(input("Player 2 Make your selection (0-" + str(COLUMN_COUNT - 1) + "): "), 10)
+
+                if 0 <= col_test and col_test < COLUMN_COUNT:
+                    col = int(col_test)
+                    false_input_count = 0
+                    break
+                
+                else:
+                    print("ERROR INPUT -> Input has to be between 0 and " + str(COLUMN_COUNT - 1))
+                    print("please try again:")
+
+            except ValueError:
+                print("ValueError")
                 print("ERROR INPUT -> Input has to be between 0 and " + str(COLUMN_COUNT - 1))
-                break
-        
-        except ValueError:
-            print("ERROR INPUT -> Input has to be a number between 0 and " + str(COLUMN_COUNT - 1))
-            break
+                print("please try again:")
+                pass
 
 
         if is_valid_location(board, col):
@@ -109,9 +130,6 @@ while not game_over:
             if winning_check(board, 2):
                 print("PLAYER 2 Wins!")    
                 game_over = True
-
-         
-
 
     print_board(board)        
 
